c916180f8356e09016568d5b3d6922f3e6c8fb7b
Your Name <you@example.com>
2025-08-03 00:22:11 -0300

    feat: added models to admin,  createdgenres entity and fixture

--- bookso/admin.py
+++ bookso/admin.py
@@ -1,4 +1,20 @@
 from django.contrib import admin
-from bookso.models import Book
 
-admin.site.register(Book)
+# from django.contrib.auth.models import User
+from .models import Book, Genre
+
+
+class GenreInline(admin.StackedInline):
+    model = Genre
+
+
+class BookAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {"fields": ["title", "language", "page_number", "genres"]}),
+        ("Date information", {"fields": ["publish_date"]}),
+    ]
+    readonly_fields = ["created_at"]
+
+
+admin.site.register(Book, BookAdmin)
+admin.site.register(Genre)
--- bookso/models.py
+++ bookso/models.py
@@ -1,22 +1,31 @@
 from django.db import models
 
 
-class Book(models.Model):
-    GENRE_CHOICES = [
-        ("romance", "Romance"),
-        ("thriller", "Terror"),
-        ("sci-fi", "Ficção Científica"),
-        ("fantasy", "Fantasia"),
-        ("adventure", "Aventura"),
-        ("self-help", "Autoajuda"),
-        ("educative", "Educativo"),
-    ]
+class Genre(models.Model):
+    name = models.CharField(max_length=150)
 
+    def __str__(self):
+        return self.name
+
+    # GENRE_CHOICES = [
+    #     ("romance", "Romance"),
+    #     ("thriller", "Terror"),
+    #     ("sci-fi", "Ficção Científica"),
+    #     ("fantasy", "Fantasia"),
+    #     ("adventure", "Aventura"),
+    #     ("self-help", "Autoajuda"),
+    #     ("educative", "Educativo"),
+    # ]
+
+
+class Book(models.Model):
     title = models.CharField(max_length=150)
-    genre = models.CharField(max_length=25, choices=GENRE_CHOICES)
+    genres = models.ManyToManyField(Genre)
     language = models.CharField(max_length=50, blank=True, default="Português")
-    publish_date = models.DateField(blank=True)
-    # created_at = models.DateTimeField()
+    publish_date = models.DateField()
+    page_number = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.title
