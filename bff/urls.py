63012282a0f81dbf952f6f152d49897c655fb07e
Your Name <you@example.com>
2025-08-07 09:10:38 -0300

    done: add token verify route

--- bff/urls.py
+++ bff/urls.py
@@ -1,14 +1,18 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from book import views
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView,
+)
 
 router = DefaultRouter()
 router.register(r"genres", views.GenreViewSet, basename="genre")
 
 urlpatterns = [
-    # path("auth/", include("rest_framework.urls")),
     path("auth/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("auth/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("auth/token/verify/", TokenVerifyView.as_view(), name="token_verify"),
     path("bff/", include(router.urls)),
 ]
