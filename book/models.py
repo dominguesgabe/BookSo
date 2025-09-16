cb92e3d0e9eaf09bab297ee9fcd890b9a3347b9e
Your Name <you@example.com>
2025-09-16 17:34:46 -0300

    improvement: book and product improvements

--- book/models.py
+++ book/models.py
@@ -9,11 +9,6 @@ class Genre(models.Model):
 
 
 class Book(models.Model):
-    BOOK_TYPE_CHOICES = [
-        ("physical", "Livro físico"),
-        ("ebook", "E-book"),
-    ]
-
     name = models.CharField(max_length=150)
     genres = models.ManyToManyField(Genre, blank=True)
     language = models.CharField(max_length=50, blank=True, default="Português")
@@ -21,7 +16,6 @@ class Book(models.Model):
     page_number = models.PositiveIntegerField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    book_type = models.CharField(max_length=10, choices=BOOK_TYPE_CHOICES)
 
     def __str__(self):
         return self.name
