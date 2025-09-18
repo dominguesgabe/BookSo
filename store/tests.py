27814c92c08c6c4f8f72ca378bef1a884e9cab4d
Your Name <you@example.com>
2025-09-17 21:08:54 -0300

    test: first testcase!

--- store/tests.py
+++ store/tests.py
@@ -1,3 +1,28 @@
 from django.test import TestCase
+from store.models import Product
+from book.models import Book, Genre
+from datetime import date
 
-# Create your tests here.
+
+class ProductTestCase(TestCase):
+    def setUp(self):
+        book = Book.objects.create(
+            name="A Fantástica Fábrica de Chocolate",
+            publish_date=date.today(),
+            page_number=100,
+        )
+
+        Genre.objects.create(name="Fantasy")
+        genre_queryset = Genre.objects.all()
+        book.genres.set(genre_queryset)
+
+        Product.objects.create(
+            book=book,
+            available_quantity=1,
+            price=10.5,
+        )
+
+    def test_check_if_book_exists(self):
+        product = Product.objects.get(pk=1)
+
+        self.assertIsInstance(product, Product)
