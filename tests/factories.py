3bedcd34e2c8adb3a34690b689468cc8374102f7
Your Name <you@example.com>
2025-09-18 19:19:37 -0300

    feat: improved factories

--- tests/factories.py
+++ tests/factories.py
@@ -9,12 +9,17 @@ def genre_factory():
     return genre
 
 
-def book_factory():
+def book_factory(
+    name="The amazing spider man - Chapter 1",
+    publish_date=date.today(),
+    page_number=125,
+    language="English",
+):
     book = Book.objects.create(
-        name="The amazing spider man - Chapter 1",
-        publish_date=date.today(),
-        page_number=100,
-        language="English",
+        name=name,
+        publish_date=publish_date,
+        page_number=page_number,
+        language=language,
     )
     genre_factory()
     genre_queryset = Genre.objects.all()
@@ -23,29 +28,17 @@ def book_factory():
     return book
 
 
-def product_factory():
-    book = book_factory()
+def product_factory(
+    book,
+    available_quantity=10,
+    price=10.5,
+    product_type=Product.PHYSICAL,
+):
     product = Product.objects.create(
         book=book,
-        available_quantity=10,
-        price=10.5,
+        available_quantity=available_quantity,
+        price=price,
+        product_type=product_type,
     )
 
     return product
-
-
-# book = Book.objects.create(
-#             name="A Fantástica Fábrica de Chocolate",
-#             publish_date=date.today(),
-#             page_number=100,
-#         )
-
-#         Genre.objects.create(name="Juvenile")
-#         genre_queryset = Genre.objects.all()
-#         book.genres.set(genre_queryset)
-
-#         Product.objects.create(
-#             book=book,
-#             available_quantity=1,
-#             price=10.5,
-#         )
